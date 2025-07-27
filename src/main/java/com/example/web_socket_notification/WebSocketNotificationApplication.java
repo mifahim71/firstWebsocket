b6329ce26f610da00295fa755d9f77e14a952ff2
Fahim <fahim.fightclub.71@gmail.com>
2025-07-27 17:59:55 +0600

    first commit, just created a spring boot porject

---
+++ src/main/java/com/example/web_socket_notification/WebSocketNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_socket_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_socket_notification/WebSocketNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_socket_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
